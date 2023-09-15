e05b0bb4336436ff05b6945072be1c1f81d04ce9
柿山幹雄 <kakicg@cgkojo.com>
2023-09-15 19:44:31 +0900

    add home header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import HomeHeader from '@/components/HomeHeader'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div className='text-red-700'>
-      Home
-    </div>
+    <header>
+      <HomeHeader />
+    </header>
   )
 }
