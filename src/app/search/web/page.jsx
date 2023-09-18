9334eb1a343965428dabfe97e6b0748fa9a47fd8
柿山幹雄 <kakicg@cgkojo.com>
2023-09-18 09:39:04 +0900

    fetch data from google search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}`)
+  const data = await response.json()
+  const results = data.items
+  console.log(searchParams)
   return (
-    <div>
-      WebSearchPage
-    </div>
+    <>
+      {results.map(result => <h1>{result.title}</h1>) }
+    </>
   )
 }
