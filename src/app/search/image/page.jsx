3eaca526316601995e41d4a131f9fafab6c3fc6c
柿山幹雄 <kakicg@cgkojo.com>
2023-09-20 08:38:53 +0900

    loading effects

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import ImageSearchResults from '@/components/ImageSearchResults'
 
 export default async function ImageSearchPage({searchParams}) {
-  // await new Promise((resolve)=>setTimeout(resolve, 5000))
+  await new Promise((resolve)=>setTimeout(resolve, 5000))
   const startIndex = searchParams.start || "1"
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image&start=${startIndex}`)
   if(!response.ok) throw new Error("Something went wrong")
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import WebSearchResults from '@/components/WebSearchResults'
 
 export default async function WebSearchPage({searchParams}) {
-  // await new Promise((resolve)=>setTimeout(resolve, 5000))
+  await new Promise((resolve)=>setTimeout(resolve, 5000))
   const startIndex = searchParams.start || "1"
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&start=${startIndex}`)
   if(!response.ok) throw new Error("Something went wrong")
